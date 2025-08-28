5547cf7f0a01bcc0385ba4c12cdb97882c6e17cc
LINESKL <priveth06@gmail.com>
2025-08-28 12:23:30 +0500

    cw-1

---
+++ Class_1/vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import Header from './components/Header/Header.jsx';
+import Footer from './components/Footer/Footer.jsx';
+import UserProfile from './components/UserProfile/UserProfile.jsx';``
+
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+
+      <UserProfile />
+
+      <Footer />
+    </div>
+  );
+}
