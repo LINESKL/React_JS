51e7360488cc3d55759953a6f17277bd7d9f23d1
LINESKL <priveth06@gmail.com>
2025-08-28 12:40:56 +0500

    cw-1

--- Class_1/vite-project/src/components/Footer/Footer.jsx
+++ Class_1/vite-project/src/components/Footer/Footer.jsx
@@ -2,26 +2,26 @@ import './Footer.css';
 
 
 export default function Footer() {
-  return (
-    <footer class="bg-white rounded-lg shadow-sm m-4 dark:bg-gray-800">
-        <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a href="#" class="hover:underline">Aliar™</a>. All Rights Reserved.
-        </span>
-        <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
-            <li>
-                <a href="#" class="hover:underline me-4 md:me-6">About</a>
-            </li>
-            <li>
-                <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
-            </li>
-            <li>
-                <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
-            </li>
-            <li>
-                <a href="#" class="hover:underline">Contact</a>
-            </li>
-        </ul>
-        </div>
-    </footer>
-  );
+    return (
+        <footer className="fixed bottom-0 left-0 w-full bg-white shadow-sm dark:bg-gray-800 z-50">
+            <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a href="#" className="hover:underline">Aliar™</a>. All Rights Reserved.
+                </span>
+                <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+                    <li>
+                        <a href="#" className="hover:underline me-4 md:me-6">About</a>
+                    </li>
+                    <li>
+                        <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
+                    </li>
+                    <li>
+                        <a href="#" className="hover:underline me-4 md:me-6">Licensing</a>
+                    </li>
+                    <li>
+                        <a href="#" className="hover:underline">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
+    );
 }
